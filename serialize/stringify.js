c0f3ea313dce45fe6fc6bce043c169df7af7a35c
Erik Dubbelboer <erik@dubbelboer.com>
2013-03-24 21:04:28 +0100

    Improved serialize testing

---
+++ serialize/stringify.js
@@ -0,0 +1,59 @@
+
+function stringify(o) {
+  if (typeof o === 'undefined') {
+    return 'undefined';
+  }
+  if (o instanceof Array) {
+    // Array.map is smart about array's with holes in them
+    // so use it when possible (not supported in IE 6,7).
+    if (o.map) {
+      return '[' + o.map(stringify).join(', ') + ']';
+    }
+
+    var r = '[';
+
+    for (var i = 0; i < o.length; ++i) {
+      r += stringify(o[i]);
+     
+      if (i < (o.length - 1)) {
+        r += ', ';
+      }
+    }
+
+    return r + ']';
+  }
+  if (o instanceof Date) {
+    return 'new Date(' + o.getTime() + ')';
+  }
+  if (typeof o === 'object') {
+    var kv = [];
+     
+    for (var p in o) {
+      if (o.hasOwnProperty(p)) {
+      kv.push(p + ': ' + stringify(o[p]));
+      }
+    }
+     
+    return '{\n' + kv.join(',\n') + '\n}';
+  }
+  if (typeof o === 'string') {
+    return '"' + o
+      .replace(/\\/g , '\\\\')
+      .replace(/\u0008/g, '\\b') // Backspace.
+      .replace(/\t/g , '\\t')
+      .replace(/\n/g , '\\n')
+      .replace(/\f/g , '\\f')
+      .replace(/\r/g , '\\r')
+      .replace(/"/g , '\\"')
+      + '"';
+  }
+   
+  return o.toString();
+}
+
+
+//<testonly>
+if (typeof window === 'undefined') {
+  module.exports = stringify;
+}
+
--- serialize/test.js
+++ serialize/test.js
@@ -1,57 +1,7 @@
 
-var assert = require('assert');
-
-
-function stringify(o) {
-  if (typeof o === 'undefined') {
-    return 'undefined';
-  }
-  if (o instanceof Array) {
-    // Array.map is smart about array's with holes in them
-    // so use it when possible (not supported in IE 6,7).
-    if (o.map) {
-      return '[' + o.map(stringify).join(', ') + ']';
-    }
-
-    var r = '[';
-
-    for (var i = 0; i < o.length; ++i) {
-      r += stringify(o[i]);
-     
-      if (i < (o.length - 1)) {
-        r += ', ';
-      }
-    }
-
-    return r + ']';
-  }
-  if (o instanceof Date) {
-    return 'new Date(' + o.getTime() + ')';
-  }
-  if (typeof o === 'object') {
-    var kv = [];
-     
-    for (var p in o) {
-      if (o.hasOwnProperty(p)) {
-      kv.push(p + ': ' + stringify(o[p]));
-      }
-    }
-     
-    return '{\n' + kv.join(',\n') + '\n}';
-  }
-  if (typeof o === 'string') {
-    return '"' + o
-      .replace(/\\/g , '\\\\')
-      .replace(/\u0008/g, '\\b') // Backspace.
-      .replace(/\t/g , '\\t')
-      .replace(/\n/g , '\\n')
-      .replace(/\f/g , '\\f')
-      .replace(/\r/g , '\\r')
-      .replace(/"/g , '\\"')
-      + '"';
-  }
-   
-  return o.toString();
+if (typeof window === 'undefined') {
+  // Only for nodejs.
+  var stringify = require('./stringify.js');
 }
 
 
@@ -80,7 +30,28 @@ for (var i = 0; i < tests.length; ++i) {
         var test;
         eval('test = ' + stringify(tests[i]));
 
-        assert.equal(JSON.stringify(test), JSON.stringify(tests[i]));
+        if (typeof tests[i] != typeof test) {
+          throw new Error('typeof mismatch');
+        }
+
+        if (typeof tests[i] == 'Object') {
+          var fk = Object.keys(tests[i]);
+          var rk = Object.keys(test);
+
+          if (fk.length != rk.length) {
+            throw new Error('object length mismatch');
+          }
+
+          for (var k = 0; k < fk.length; ++k) {
+            if (tests[fk[k]] !== test[rk[k]]) {
+              throw new Error('mismatch on propery ' + k);
+            }
+          }
+        } else if (typeof tests[i] != 'undefined') {
+          if (('' + tests[i]) != ('' + test)) {
+            throw new Error('toString mismatch');
+          }
+        }
       });
     });
   })(i);
