bdf9db031e16b610031682a26d2f21baa95ae7cc
Erik Dubbelboer <erik@dubbelboer.com>
2013-03-26 18:00:08 +0100

    fixing testling

--- serialize/test.js
+++ serialize/test.js
@@ -1,5 +1,57 @@
 
-var stringify = require('./stringify.js');
+//var stringify = require('./stringify.js');
+function stringify(o) {
+  if (typeof o === 'undefined') {
+    return 'undefined';
+  }
+  if (o instanceof Array) {
+    // Array.map is smart about array's with holes in them
+    // so use it when possible (not supported in IE 6,7).
+    if (o.map) {
+      return '[' + o.map(stringify).join(', ') + ']';
+    }
+
+    var r = '[';
+
+    for (var i = 0; i < o.length; ++i) {
+      r += stringify(o[i]);
+
+      if (i < (o.length - 1)) {
+        r += ', ';
+      }
+    }
+
+    return r + ']';
+  }
+  if (o instanceof Date) {
+    return 'new Date(' + o.getTime() + ')';
+  }
+  if (typeof o === 'object') {
+    var kv = [];
+
+    for (var p in o) {
+      if (o.hasOwnProperty(p)) {
+      kv.push(p + ': ' + stringify(o[p]));
+      }
+    }
+
+    return '{\n' + kv.join(',\n') + '\n}';
+  }
+  if (typeof o === 'string') {
+    return '"' + o
+      .replace(/\\/g , '\\\\')
+      .replace(/\u0008/g, '\\b') // Backspace.
+      .replace(/\t/g , '\\t')
+      .replace(/\n/g , '\\n')
+      .replace(/\f/g , '\\f')
+      .replace(/\r/g , '\\r')
+      .replace(/"/g , '\\"')
+      + '"';
+  }
+
+  return o.toString();
+}
+
 
 var tests = [
   1,
